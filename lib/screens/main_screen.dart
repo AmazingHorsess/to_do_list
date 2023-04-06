2a91bd33ca58c786d54fa6c575ceeef678e936b6
AmazingHorsess <amazinghorsess@gmail.com>
2023-04-07 00:51:13 +0600

    ToDo Item implementation

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,31 +1,94 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_list/constants/colors.dart';
 
+import '../widgets/todo_item.dart';
+
 class Home extends StatelessWidget{
   const Home ({Key? key}) :super(key: key);
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: tdBGColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Icon(Icons.menu, color: tdBlack, size: 35,
-            ),
-            Container(
-              height: 40,
-              width: 40,
-              child: const Icon(Icons.supervised_user_circle_rounded, color: tdGrey, size: 35,),
-            )
+      appBar: __buildAppBar(),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20,vertical: 15),
+        child: Column(children: [
+          searchBox(),
+          Expanded(
+            child: ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 50,bottom: 20),
+                  child:
+                  Text("All ToDos",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500
+                    ),
+                  ),
+          ),
+                  ToDoItem(),
+                ToDoItem(),
+                ToDoItem(),
+                ToDoItem(),
+                ToDoItem(),
+                ToDoItem(),
 
-          ],
+
+
+
+              ],
+            ),
+          )
+
+        ],
+        ),
+      ),
+    );
+  }
+
+  Container searchBox() {
+    return Container(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          decoration: BoxDecoration(
+          color: Colors.white,
+        borderRadius: BorderRadius.circular(20)
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(0),
+            prefixIcon: Icon(Icons.search, color: tdBlack, size: 20,
+            ),
+            prefixIconConstraints: BoxConstraints(maxHeight: 20,minWidth: 25
+            ),
+            border: InputBorder.none,
+            hintText: "Search",
+            hintStyle: TextStyle(color: tdGrey)
+          ),
         ),
 
+        );
+  }
+
+
+
+
+  AppBar __buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Icon(Icons.menu, color: tdBlack, size: 35,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: const Icon(Icons.supervised_user_circle_rounded, color: tdGrey, size: 35,),
+          )
+
+        ],
       ),
-      body: Container(
-        child: Text("check"),
-      ),
+
     );
   }
 
---
+++ lib/widgets/todo_item.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list/constants/colors.dart';
+
+class ToDoItem extends StatelessWidget{
+  const ToDoItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      margin: EdgeInsets.only(bottom: 20),
+      child: ListTile(
+        onTap: () {
+
+        },
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)
+        ),
+        tileColor: Colors.white,
+        contentPadding: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
+        leading: Icon(Icons.check_box, color: tdBlue,),
+        title: Text('Check Mail',style: TextStyle(fontSize: 16,color: tdBlack, decoration: TextDecoration.lineThrough),),
+        trailing: Container(
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.symmetric(vertical:5),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: tdRed,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: IconButton(
+            color: Colors.white,
+            iconSize: 18,
+            icon: Icon(Icons.delete),
+            onPressed: (){},
+          ),
+        ),
+
+      ),
+    );
+
+
+  }
+}
